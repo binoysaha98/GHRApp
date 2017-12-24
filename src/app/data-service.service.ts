07fbe6f833efe785dd825be041fcc33c68912ef9
binoysaha98 <binoysaha98@gmail.com>
2017-12-24 18:58:56 +0530

    made changes

--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -10,20 +10,20 @@ export class DataService {
   addUserEmail(user){
 	  var headers = new Headers();
 	  headers.append("content-type","application/json");
-	return this._http.post("http://localhost:3000/createUserEmail",user,{headers:headers}).map(res => res.json());
+	return this._http.post("https://secure-sea-22568.herokuapp.com/createUserEmail",user,{headers:headers}).map(res => res.json());
   }
   
   addUserPhone(user){
 	  var headers = new Headers();
 	  headers.append("content-type","application/json");
-	return this._http.post("http://localhost:3000/createUserPhone",user,{headers:headers}).map(res => res.json());
+	return this._http.post("https://secure-sea-22568.herokuapp.com/createUserPhone",user,{headers:headers}).map(res => res.json());
   }
   
   authenticateUser(user)
   {
 	  var headers = new Headers();
 	  headers.append("content-type","application/json");
-	return this._http.post("http://localhost:3000/authenticateUser",user,{headers:headers}).map(res => res.json());
+	return this._http.post("https://secure-sea-22568.herokuapp.com/authenticateUser",user,{headers:headers}).map(res => res.json());
   }
   
   isLoggedIn(){
@@ -38,7 +38,7 @@ export class DataService {
 		 return false; 
 	  }
 	  else{
-		return this._http.post("http://localhost:3000/isLoggedIn",data,{headers:headers}).map(res => res.json().result);
+		return this._http.post("https://secure-sea-22568.herokuapp.com/isLoggedIn",data,{headers:headers}).map(res => res.json().result);
 	  }
 	}
 	
@@ -54,7 +54,7 @@ export class DataService {
 		 return false; 
 	  }
 	  else{
-		return this._http.post("http://localhost:3000/isDoctor",data,{headers:headers}).map(res => res.json().doctor);
+		return this._http.post("https://secure-sea-22568.herokuapp.com/isDoctor",data,{headers:headers}).map(res => res.json().doctor);
 	  }
 	}
 	
@@ -64,7 +64,7 @@ export class DataService {
 	  var data = {
 		  number : number
 	  };
-	  return this._http.post("http://localhost:3000/getSpecifiedUser",data,{headers:headers}).map(res => res.json());
+	  return this._http.post("https://secure-sea-22568.herokuapp.com/getSpecifiedUser",data,{headers:headers}).map(res => res.json());
 	}
 
 	getUser(){
@@ -74,7 +74,7 @@ export class DataService {
 	  var data = {
 		  token : token
 	  };
-	  return this._http.post("http://localhost:3000/isLoggedIn",data,{headers:headers}).map(res => res.json());
+	  return this._http.post("https://secure-sea-22568.herokuapp.com/isLoggedIn",data,{headers:headers}).map(res => res.json());
 	  
 	}
 	
@@ -82,50 +82,50 @@ export class DataService {
 	sendSMS(email){
 		var headers = new Headers();
 	    headers.append("content-type","application/x-www-form-urlencoded");
-		return this._http.post("http://localhost:80/SMS/sendSMS.php",email,{headers:headers}).map(res => res.json());
+		return this._http.post("http://ghr1.epizy.com/SMS/sendSMS.php",email,{headers:headers}).map(res => res.json());
 	}
 	
 	sendMail(email){
 		var headers = new Headers();
 	    headers.append("content-type","application/json");
-		return this._http.post("http://localhost:3000/sendMail",email,{headers:headers}).map(res => res.json());
+		return this._http.post("https://secure-sea-22568.herokuapp.com/sendMail",email,{headers:headers}).map(res => res.json());
 	
 	}
 	
 	verifyDoctor(user){
 		var headers = new Headers();
 	    headers.append("content-type","application/json");
-		return this._http.post("http://localhost:3000/verifyDoctor",user,{headers:headers}).map(res => res.json());
+		return this._http.post("https://secure-sea-22568.herokuapp.com/verifyDoctor",user,{headers:headers}).map(res => res.json());
 	
 	}
 	
 	sendNotification(){
 		var headers = new Headers();
 	    headers.append("content-type","application/json");
-		return this._http.post("http://localhost:80/SMS/send_message.php",{},{headers:headers}).map(res => res.json());
+		return this._http.post("http://ghr1.epizy.com/SMS/send_message.php",{},{headers:headers}).map(res => res.json());
 	}
 	
 	sendNoti(body){
 		var headers = new Headers();
 	    headers.append("content-type","application/json");
-		return this._http.post("http://localhost:3000/sendNoti",body,{headers:headers}).map(res => res.json());
+		return this._http.post("https://secure-sea-22568.herokuapp.com/sendNoti",body,{headers:headers}).map(res => res.json());
 	}
 	
 	subscribeTopic(body){
 		var headers = new Headers();
 	    headers.append("content-type","application/json");
-		return this._http.post("http://localhost:3000/subscribeToTopic",body,{headers:headers}).map(res => res.json());
+		return this._http.post("https://secure-sea-22568.herokuapp.com/subscribeToTopic",body,{headers:headers}).map(res => res.json());
 	}
 	
 	setTimerChatRoom(body){
 		var headers = new Headers();
 	    headers.append("content-type","application/json");
-		return this._http.post("http://localhost:3000/setTimerChatRoom",body,{headers:headers}).map(res => res.json());
+		return this._http.post("https://secure-sea-22568.herokuapp.com/setTimerChatRoom",body,{headers:headers}).map(res => res.json());
 	}
 	getCurrentTime(){
 		var headers = new Headers();
 	    headers.append("content-type","application/json");
-		return this._http.post("http://localhost:3000/getCurrentTime",{},{headers:headers}).map(res => res.json());
+		return this._http.post("https://secure-sea-22568.herokuapp.com/getCurrentTime",{},{headers:headers}).map(res => res.json());
 	
 	}
 }
